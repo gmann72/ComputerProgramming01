a38d8f36ef8d458c9d4d17c1c73311120294df83
Mann <G.Mann7769@student.leedsbeckett.ac.uk>
2023-10-12 10:58:53 +0100

    Message

---
+++ Week 1/Portfolio.1.py
@@ -0,0 +1,2 @@
+print("Hello gmann")
+
---
+++ Week 2/Lab.2.py
@@ -0,0 +1,15 @@
+total = 100
+print("The total is", total)
+
+total = total + 99
+print("The total is", total)
+
+total = total - 1
+print("The total is", total)
+
+total = total * 4
+print("The total is", total)
+
+total = total / 2
+print("The total is", total)
+
