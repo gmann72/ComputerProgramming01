3ddf3c1ce05176352edee0051431182b70ab722a
gmann72 <G.Mann7769@student.leedsbeckett.acuk>
2023-10-24 11:23:50 +0100

    Week 1

--- Week 1/Portfolio.1.py
+++ Week 1/Portfolio.1.py
@@ -1,2 +1,1 @@
-print("Hello gmann")
-
+print("Hello World")
---
+++ Week 1/Portfolio1b.py
@@ -0,0 +1,9 @@
+# Here we will define the temp
+celsius_temperature = 38.4
+
+# Converting Celsius to Fahrenheit using the conversion formula
+# This formula converts the temperature from Celsius to Fahrenheit.
+fahrenheit_temperature = (celsius_temperature * 9/5) + 32
+
+# Display the temperatures
+print(f"{celsius_temperature} degrees Celsius is equal to {fahrenheit_temperature} degrees Fahrenheit")
