c6eefc1bf7078ef6474fef4080b07e16f9965500
gmann72 <G.Mann7769@student.leedsbeckett.acuk>
2023-10-24 12:28:16 +0100

    Week 2

--- Week 2/Lab.2.CallingInBuiltFunctions.py
+++ Week 2/Lab.2.CallingInBuiltFunctions.py
@@ -1,3 +1,18 @@
-name = len("Gurveer Mann")
-print("The length of you name is" + len(name))
+# Enter your information
+name = "Gurveer Mann"
+address = "Leeds Beckett University, LS6 3QS"
+contact_details = "+44 (0)113 812 5375"
+
+# Display your name, address, and contact details (whoever you are)
+print("Name:", name)
+print("Address:", address)
+print("Contact Details:", contact_details)
+
+# Calculate and print the length of your name
+name_length = len(name)
+print("Length of Name:", name_length)
+# The length of the name "Gurveer Mann", if you just count the letters are 11
+# However, the space (" ") in between "Gurveer" and "Mann" counts towards the length of the name
+# That's why it is shown as 12 and not 11, if you remove the space from the "Enter your information" section
+# It will calculate the length of the name as 11 characters and not 12
 
--- Week 2/Lab.2.DataTypes.py
+++
@@ -1,8 +0,0 @@
-type(False)
-type(1000)
-type(100.111)
-type("Hello")
-type(True)
-type(100 / 5)
-type(100 // 5)
-
